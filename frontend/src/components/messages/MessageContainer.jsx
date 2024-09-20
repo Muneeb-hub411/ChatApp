c6a6d1884c4af0557ed8d0f473f1cfc5d23e0933
Muneeb-hub411 <muneebsaleem402@gmail.com>
2024-09-20 23:27:40 +0500

    improved the UI

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -3,12 +3,12 @@ import Messages from "./Messages";
 
 const MessageContainer = () => {
   return (
-    <div className="md:min-w-[450px] flex flex-col max-h-[600px]">
+    <div className="md:min-w-[450px] flex flex-col max-h-[525px] bg-gradient-to-b from-slate-700 to-slate-800 text-white">
       <>
         {/* Header */}
-        <div className="bg-slate-500 px-4 py-2 mb-2">
-          <span className="label-text">To:</span>{" "}
-          <span className="text-gray-900 font-bold">John doe</span>
+        <div className="bg-slate-600 px-4 py-2 mb-2">
+          <span className="text-slate-300">To:</span>{" "}
+          <span className="text-white font-bold">John doe</span>
         </div>
 
         {/* Messages */}
@@ -22,4 +22,5 @@ const MessageContainer = () => {
     </div>
   );
 };
+
 export default MessageContainer;
--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -4,12 +4,21 @@ import SearchInput from "./SearchInput";
 
 const Sidebar = () => {
   return (
-    <div className="border-r border-slate-500 p-4 flex flex-col">
-      <SearchInput />
-      <div className="divider px-3"></div>
-      <Conversations />
-      <LogoutButton />
+    <div className="border-r border-slate-300 flex flex-col max-h-[525px] bg-gradient-to-b from-slate-800 to-slate-900 text-white">
+      <div className="p-4">
+        <SearchInput />
+      </div>
+      <div className="divider px-3 my-2 border-t border-slate-600"></div>
+      <div className="flex-grow overflow-hidden">
+        <div className="h-full overflow-y-auto px-4">
+          <Conversations />
+        </div>
+      </div>
+      <div className="p-4">
+        <LogoutButton />
+      </div>
     </div>
   );
 };
+
 export default Sidebar;
